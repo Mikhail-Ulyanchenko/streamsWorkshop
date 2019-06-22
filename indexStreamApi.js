5ed6de49163c650d8166287dae85f23205f9c66b
mykhailou <mykhailou@wix.com>
2019-06-23 01:31:27 +0300

    squash to one file bigFileServer

--- indexStreamApi.js
+++ indexStreamApi.js
@@ -1,15 +1,28 @@
-const fs = require( 'fs' );
-const server = require( 'http' ).createServer();
-const request = require( 'request' );
+const fs = require('fs');
+const request = require('request');
+const apiServer = require('http').createServer();
+const sourceServer = require('http').createServer();
 
-server.on( 'request', ( req, res ) => {
-  const readStream = request.get( 'http://127.0.0.1:1111' );
 
-  readStream.on( 'data', () => {
-    console.log( process.memoryUsage() );
-  } );
+const API_SERVER_PORT = 8080;
+const READER_SERVER_PORT = 8081;
 
-  readStream.pipe( res );
-} );
+apiServer.on('request', (req, res) => {
+    const readStream = request.get(`http://127.0.0.1:${READER_SERVER_PORT}`);
 
-server.listen( 8080 );
+    readStream.on('data', () => {
+        console.log(process.memoryUsage());
+    });
+
+    readStream.pipe(res);
+});
+
+apiServer.listen(API_SERVER_PORT);
+
+
+sourceServer.on('request', (req, res) => {
+    const src = fs.createReadStream('./big.file');
+    src.pipe(res);
+});
+
+sourceServer.listen(READER_SERVER_PORT);
