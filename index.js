93c07a7e2433629f086c8eaa13e6098c5190229b
mykhailou <mykhailou@wix.com>
2019-06-23 12:23:58 +0300

    parse large json

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const fs = require( 'fs' );
-const server = require( 'http' ).createServer();
+const fs = require('fs');
+const server = require('http').createServer();
 
-server.on( 'request', ( req, res ) => {
-  fs.readFile( './big.file', ( err, data ) => {
-    if ( err ) throw err;
+server.on('request', (req, res) => {
+    fs.readFile('./big.file', (err, data) => {
+        if (err) throw err;
+        console.log(process.memoryUsage());
 
-    res.end( data );
-  } );
-} );
+        res.end(data);
+    });
+});
 
-server.listen( 8000 );
+server.listen(8080);
--- indexStreamApi.js
+++ indexStreamApi.js
@@ -6,23 +6,30 @@ const sourceServer = require('http').createServer();
 
 const API_SERVER_PORT = 8080;
 const READER_SERVER_PORT = 8081;
-
-apiServer.on('request', (req, res) => {
-    const readStream = request.get(`http://127.0.0.1:${READER_SERVER_PORT}`);
-
-    readStream.on('data', () => {
-        console.log(process.memoryUsage());
-    });
-
-    readStream.pipe(res);
-});
-
-apiServer.listen(API_SERVER_PORT);
+//
+// apiServer.on('request', (req, res) => {
+//     const readStream = request.get(`http://127.0.0.1:${READER_SERVER_PORT}`);
+//
+//     readStream.on('data', () => {
+//         console.log(process.memoryUsage());
+//     });
+//
+//     readStream.pipe(res);
+// });
+//
+// apiServer.listen(API_SERVER_PORT);
 
 
 sourceServer.on('request', (req, res) => {
     const src = fs.createReadStream('./big.file');
+
+    src.on('data', () => {
+        console.log(process.memoryUsage());
+    });
+
     src.pipe(res);
 });
 
-sourceServer.listen(READER_SERVER_PORT);
+sourceServer.listen(API_SERVER_PORT);
+
+console.log(`API server is listening on port number: ${API_SERVER_PORT}`);
